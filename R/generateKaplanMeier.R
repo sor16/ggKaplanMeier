8507836fbc52850aef12a091eefe8d8c295805db
sor16 <solviro@gmail.com>
2016-05-19 11:11:42 +0000

    New function which generates Kaplan Meier plots from multiple variables in a data frame

---
+++ R/generateKaplanMeier.R
@@ -0,0 +1,16 @@
+generateKaplanMeier <- function(data,surv_object,wantedColumns,path,startAt=1,...){
+    require(survival)
+    require(devtools)
+    require(ggKaplanMeyer)
+    setwd(path)
+    wantedColumns <- paste("^",wantedColumns,"$",sep="") %>% paste(collapse = "|")
+    plotData <- data %>% select(matches(wantedColumns))
+    lapply(startAt:ncol(plotData),function(i) {
+        formula <- as.formula(paste("surv_object",names(plotData)[i],sep="~"))
+        fit <- survfit(formula, data=plotData)
+        pdf(file=paste(names(plotData)[i],"pdf",sep="."), width =16 , height = 9.6,onefile=FALSE)
+        grid.newpage()
+        grid.draw(addrisk(gg_KM(fit,...)))
+        dev.off()
+    })
+}
